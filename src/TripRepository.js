2548d02d6be726ee6d2a9cf551796d20ff35583b
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-24 14:27:55 -0700

    Add get trips method to user

--- src/TripRepository.js
+++ src/TripRepository.js
@@ -1,3 +1,16 @@
-export default class TripRepository {
+import Trip from './Trip';
 
+export default class TripRepository {
+  static trips = [];
+  static getTrips(){
+    if(TripRepository.trips.length > 0){
+      return Promise.resolve(TripRepository.trips);
+    }
+    return fetch('http://localhost:3001/api/v1/trips')
+      .then(response => response.json())
+      .then(data => {
+        TripRepository.trips = data.map(trip => new Trip(trip));
+        return TripRepository.trips;
+      });
+  }
 }
--- src/User.js
+++ src/User.js
@@ -1,22 +1,10 @@
-import Trip from './Trip';
+import TripRepository from './TripRepository';
 
 export default class User {
   constructor(user) {
     Object.assign(this, user);
-    this.trips = [];
   }
   getTrips(){
-    if(this.trips.length > 0){
-      return Promise.resolve(this.trips);
-    }
-    return fetch('http://localhost:3001/api/v1/trips')
-      .then(response => response.json())
-      .then(data => {
-        this.trips = data.filter(trip => trip.userID === this.id).map(trip => new Trip(trip));
-        return this.trips;
-      });
-  }
-  getTotalAmountSpent() {
-
+    return TripRepository.getTrips().then(data => data.filter(trip => trip.userID === this.id))
   }
 }
