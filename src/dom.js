1ebdafe4378cd85040efe15fe6cbd68f4cf746da
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-24 16:03:58 -0700

    Add first dom update

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,18 @@
+import State from './State';
+
 const travelerData = document.getElementById('travelerData');
 
 function render(){
 
 }
 
-function traveler(){
-
+async function renderTraveler(){
+  let htmlData = "<ul>";
+  const trips = await State.currentUser.getTrips();
+  trips.forEach(async trip => {
+    const destination = await trip.getDestination();
+    htmlData += `<li>${destination.destination}</li>`
+  });
+  htmlData += "</ul>";
+  travelerData.innerHTML = htmlData;
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './dom';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
