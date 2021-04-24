3ec194c2526016a6b7ff466a50ed140a53cd754c
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-24 14:19:42 -0700

    Add destination repository functionality

--- src/Destination.js
+++ src/Destination.js
@@ -1,10 +1,5 @@
 export default class Destination {
-  constructor(id, destination, estimatedLodgingCost, estimatedFlightCost, image, alt) {
-    this.id = id;
-    this.destination = destination;
-    this.estimatedLodgingCost = estimatedLodgingCost;
-    this.estimatedFlightCost = estimatedFlightCost;
-    this.image = image;
-    this.alt = alt;
+  constructor(destination) {
+    Object.assing(this, destination);
   }
 }
--- src/User.js
+++ src/User.js
@@ -1,7 +1,22 @@
+import Trip from './Trip';
+
 export default class User {
-  constructor(id, name, type) {
-    user.id = id;
-    user.name = name;
-    user.type = type;
+  constructor(user) {
+    Object.assign(this, user);
+    this.trips = [];
+  }
+  getTrips(){
+    if(this.trips.length > 0){
+      return Promise.resolve(this.trips);
+    }
+    return fetch('http://localhost:3001/api/v1/trips')
+      .then(response => response.json())
+      .then(data => {
+        this.trips = data.filter(trip => trip.userID === this.id).map(trip => new Trip(trip));
+        return this.trips;
+      });
+  }
+  getTotalAmountSpent() {
+
   }
 }
