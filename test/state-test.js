90816d33c1189625d93987760bad09aeda37a291
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-26 15:33:37 -0700

    Add State testing

--- test/state-test.js
+++ test/state-test.js
@@ -1,11 +1,22 @@
 import chai from 'chai';
+import State from '../src/State.js';
 const expect = chai.expect;
+var assert = require('chai').assert;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+  describe('State', function() {
+
+    it('should have a current user by default be null', function() {
+      var state = new State();
+
+      assert.equal(state.currentUser, null);
+
   });
 
-//is class
+    it('should have a current page by default be undefined', function() {
+      var state = new State();
+
+      assert.equal(state.currentPage, undefined);
+
+  });
 
 });
