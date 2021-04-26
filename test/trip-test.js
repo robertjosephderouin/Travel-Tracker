c24e3eb241595e2d84d1fa0dfa31128a6c8d7ead
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-26 15:49:43 -0700

    Add trip testing

--- test/trip-test.js
+++ test/trip-test.js
@@ -1,15 +1,69 @@
 import chai from 'chai';
+import Trip from '../src/Trip.js';
 const expect = chai.expect;
+var assert = require('chai').assert;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+const sampleTrip = {
+            "id": 1,
+            "userID": 44,
+            "destinationID": 49,
+            "travelers": 1,
+            "date": "2019/09/16",
+            "duration": 8,
+            "status": "approved",
+            "suggestedActivities": []
+            }
+
+  describe('Trip', function() {
+
+    it('should have a id', function() {
+      var trip = new Trip(sampleTrip);
+
+      assert.equal(trip.id, '1');
+
   });
 
-//constructor
+    it('should have a user id', function() {
+    var trip = new Trip(sampleTrip);
 
-//getDestination()
+    assert.equal(trip.userID, '44');
 
-//getCost
+  });
+
+
+    it('should have a destination ID', function() {
+    var trip = new Trip(sampleTrip);
+
+    assert.equal(trip.destinationID, '49');
+
+  });
+
+    it('should have a number travelers', function() {
+    var trip = new Trip(sampleTrip);
+
+    assert.equal(trip.travelers, 1);
+
+  });
+
+    it('should have a date', function() {
+    var trip = new Trip(sampleTrip);
+
+    assert.equal(trip.date, "2019/09/16");
+
+  });
+
+    it('should have a duration', function() {
+    var trip = new Trip(sampleTrip);
+
+    assert.equal(trip.duration, 8);
+
+  });
+
+    it('should have a status', function() {
+    var trip = new Trip(sampleTrip);
+
+    assert.equal(trip.status, "approved");
+
+  });
 
 });
