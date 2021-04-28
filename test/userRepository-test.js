e2899da0b9f15717e802f261dca0fc0b47c0d6ee
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-27 17:16:29 -0700

    Refactor UserRepository Tests

--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -1,15 +1,64 @@
 import chai from 'chai';
+import User from'../src/User';
+import UserRepository from'../src/UserRepository';
 const expect = chai.expect;
+var assert = require('chai').assert;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('UserRepository', function() {
+
+  it('it should return a number of users equal to the data length', async function() {
+      UserRepository.users = [new User(
+        {
+            "id": 1,
+            "name": "Ham Leadbeater",
+            "travelerType": "relaxer"
+        })]
+      const sampleGetUsers = await UserRepository.getUsers();
+      assert.equal(sampleGetUsers.length, 1);
   });
 
-// is class
+  it('it should return a number of users equal to the data length', async function() {
+      UserRepository.users = [new User(
+        {
+            "id": 1,
+            "name": "Ham Leadbeater",
+            "travelerType": "relaxer"
+        })]
+      const sampleGetUsers = await UserRepository.getUsers();
+      assert.instanceOf(sampleGetUsers[0], User);
+  });
 
-// getUsers
+  it('it should return a specific instance of user via ID and get their name', async function() {
+      UserRepository.users = [new User(
+        {
+            "id": 1,
+            "name": "Ham Leadbeater",
+            "travelerType": "relaxer"
+        }),
+        new User({
+            "id": 2,
+            "name": "Rachael Vaughten",
+            "travelerType": "thrill-seeker"
+        })]
+      const sampleGetUser = await UserRepository.getUser(2);
+      assert.instanceOf(sampleGetUser, User);
+      assert.equal(sampleGetUser.name, "Rachael Vaughten");
+  });
 
-// getUser
+  it('it should return undefined if no user id exists', async function() {
+      UserRepository.users = [new User(
+        {
+            "id": 1,
+            "name": "Ham Leadbeater",
+            "travelerType": "relaxer"
+        }),
+        new User({
+            "id": 2,
+            "name": "Rachael Vaughten",
+            "travelerType": "thrill-seeker"
+        })]
+      const sampleGetUser = await UserRepository.getUser(0);
+      assert.equal(sampleGetUser, undefined);
+  });
 
 });
